2140f244397e6bd5ee2ee013eb31e518e373a587
Mano <Koutouzosm@gmail.com>
2019-05-08 16:19:41 -0400

    added config and connection to database

---
+++ DB/seeds.sql
@@ -0,0 +1,9 @@
+USE burgers_db;
+
+INSERT INTO burgers
+  (burger_name)
+VALUES
+  ("Bacon Cheeseburger"),
+  ("California Cheeseburger"),
+  ("Texas BBQ Burger"),
+  ("Bluecheese Burger");
---
+++ config/connection.js
@@ -0,0 +1,27 @@
+const mysql = require("mysql");
+require("dotenv").config();
+
+let connection;
+
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+}
+else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "process.env.PW",
+    database: "burgers_db"
+  });
+}
+
+connection.connect(function(err) {
+    if (err){
+      console.log(err);
+      return
+    }
+    console.log("Connected as id " + connection.threadId)
+  });
+
+module.exports = connection;
